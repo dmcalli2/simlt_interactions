45aa86c5db3292f73ae151507f0cb0cd05bb6a96
David A McAllister <David.McAllister@glasgow.ac.uk>
2017-11-03 16:34:10 +0000

    Begin updating simulation as per diabetes data

--- 00_read_data.r
+++ 00_read_data.r
@@ -1,31 +1,9 @@
 # 00 Read in data
-library(readr)
-library(stringr)
-library(dplyr)
-library(tidyr)
+library(tidyverse)
 
-## Read in csv with selected trials
-trials <- read_csv(file = "../export_r/Summary conditions and treatments reviewed_Main.csv", col_types = paste(rep("c",17),collapse = "") )
-trials$drug_group_allocation [trials$drug_group_allocation == "Haemostatsis"] <- "Haemostasis"
+## Read in diabetes trials
+load("../Trial_identify/clinical_trials_august_2017/scratch_data/data_for_simulation.Rdata")
 
-## Select non discontinued drug types - 171 trials
-trials <- filter (trials, ! bnf_group %in% c("Discontinued", "0"))
 
-## Read in number of participants
-clinical_study <- read_delim("../AACT201409Annual_Pipe_delimited_txt/clinical_study_noclob_nolf.txt", delim = "|")  
-names(clinical_study) <- tolower(names(clinical_study))
-
-clinical_study <- filter (clinical_study, nct_id %in% trials$nct_id)
-
-## Sample of NCTID by sponsor
-set.seed(1234)
-sponsor_sample <- group_by(clinical_study, source)
-sponsor_sample <- sample_n(sponsor_sample, size = 1)
-# Most had data on websites (75%)
-
-## Join
-trials <- inner_join(trials, clinical_study[ , c("nct_id", "enrollment")])
-rm(clinical_study)
-gc()
-
-save (trials, file = "Dataset_of_trials.Rdata")
+diabetes_final <- diabetes_final %>% 
+  filter(atc_5 != "A10BF")
--- 01_one_time_samples.R
+++ 01_one_time_samples.R
@@ -3,101 +3,140 @@
 # Next Calculates overall group effect and variance for each component that will make-up the final scenarios
 # eg (intercept, comorbidity effect, treatment allocation and interaction)
 
+## Read in diabetes trials
+load("../Trial_identify/clinical_trials_august_2017/scratch_data/data_for_simulation.Rdata")
 
-############## 1 Make samples for all scenarios
-## Make same answer each time
-set.seed(234)
+# Remove single alpha-glucosidase inhibitor trial
+diabetes_final <-  
+  subset(diabetes_final, diabetes_final$atc_5 != "A10BF")
 
-### Determine number of samples needed for each scenario, do random sampling once for all analyses
-### for all drug_classes
-datasets <- 1000
-drug_classes <- 6 # maximum number of classes
-trials <- 10 # maximum number of trials per drug class
 
-# Loop through parameters to keep it manageable, even though sampling 4 times
-params <-  4 # intercept, treatment, 1 x comorbidities, 1 x interactions
+## Make same answer each set of classes (not each class)
+set.seed(1234)
 
-param_smpls <- lapply(1:params, function (x) {
-  ### Create array for classes, note 10 identical slices
-  dc_var <-
-    matrix(rnorm(datasets * drug_classes), ncol = drug_classes)
-  dc_var <- lapply(1:trials, function(x)
-    dc_var)
-  dc_var <- sapply(dc_var, identity, simplify = "array")
-  
-  ## Create array for trials
-  trl_var <-
-    array(rnorm(datasets * drug_classes * trials),
-          dim = c(datasets, drug_classes, trials))
-  
-  list(dc_var = dc_var, trl_var = trl_var)
-})
-names(param_smpls) <- c("intrcpt", "alloc", "com1", "intrct1")
 
-# Make all samples positive
-param_smpls <- lapply(param_smpls, function(x) lapply(x, abs))
-
-## A few seconds have all samples need for each trial, and it is only 4MB
-## It is 72,000 samples from random normal distributions,
-## some of which (the drug class ones) are repeated
 
 ### Each simulation scenario, overall effects
 main_scen <- expand.grid(
-  intrcpt = 0,
-  com1    = c(-0.2,-0.1, 0, 0.1, 0.2),
-  alloc   = c(-0.2,-0.1, 0, 0.1, 0.2),
-  intrct1 = c(-0.2,-0.1, 0, 0.1, 0.2)
+  cept = 0,
+  como = c(-0.2,-0.1, 0, 0.1, 0.2),
+  allc = c(-0.2,-0.1, 0, 0.1, 0.2),
+  actn = c(-0.2,-0.1, 0, 0.1, 0.2)
 )
 
-### Each simulation scenario, variation at class and trial level
-varn_scen <- list(
-  intrcpt = expand.grid(btwn_class = c(0.25, 0.5),
-                      btwn_trial = c(0.25, 0.5)),
-## Comorbidity effect 1
-  com1    = expand.grid(btwn_class = c(0.25, 0.5),
-                      btwn_trial = c(0.25, 0.5)),
-## Allocation
-  alloc   = expand.grid(btwn_class = c(0.05, 0.10, 0.15, 0.20, 0.25),
-                      btwn_trial = c(0.05, 0.10, 0.15, 0.20, 0.25)),
-## Interactions effect
-  intrct1 = expand.grid(btwn_class = c(0.05, 0.10, 0.15, 0.20, 0.25),
-                      btwn_trial = c(0.05, 0.10, 0.15, 0.20, 0.25)))
+varn_scen <- list(cept = c(0.25, 0.5),
+                     como = c(0.25, 0.5),
+                     allc = c(0.05, 0.10, 0.15, 0.20, 0.25),
+                     actn = c(0.05, 0.10, 0.15, 0.20, 0.25))
 
-## Component of linear predictor for combinations of variance, 1000 samples for each scenario
-if(!all(names(varn_scen) %in% names(param_smpls))) print ("Warning mismatch in names")
-varn_res <- lapply(names(varn_scen), function (param){ ## Loop through each paramter
-  lapply(seq_along(varn_scen[[param]]$btwn_class), function(i){ ## Loop through each combination
-      varn_scen[[param]]$btwn_class[i] * param_smpls[[param]]$dc_var + # between class
-      varn_scen[[param]]$btwn_trial[i] * param_smpls[[param]]$trl_var  # between trial
-  })
+### Determine number of samples needed for each scenario, do random sampling once for all analyses
+### for all drugs, classes and trial
+
+# Simulate variation around wider group (A10B) effect at trial, drug and class level
+SampleVarn <- function(item, smpl_lvls, n_iter = 1000){
+  my_mtrx <- matrix(nrow = length(item) * n_iter,
+                    ncol = length(smpl_lvls))
+  
+  for(j in seq_along(smpl_lvls)){
+    my_mtrx[,j] <- rnorm(length(item) * n_iter, 0, smpl_lvls[j])
+  }
+  rownames(my_mtrx) <- rep(item, n_iter)
+  colnames(my_mtrx) <- smpl_lvls
+  my_mtrx
+}
+trial <- lapply(list(cept = c(0.25, 0.5),
+                     como = c(0.25, 0.5),
+                     allc = c(0.05, 0.10, 0.15, 0.20, 0.25),
+                     actn = c(0.05, 0.10, 0.15, 0.20, 0.25)),
+                function(x) {
+                  SampleVarn(diabetes_final$nct_id, x)
+                }
+)
+
+drug <- lapply(list(cept = c(0.25, 0.5),
+                     como = c(0.25, 0.5),
+                     allc = c(0.05, 0.10, 0.15, 0.20, 0.25),
+                     actn = c(0.05, 0.10, 0.15, 0.20, 0.25)),
+                function(x) {
+                  SampleVarn(unique(diabetes_final$drug), x)
+                }
+)
+
+atc5 <- lapply(list(cept = c(0.25, 0.5),
+                     como = c(0.25, 0.5),
+                     allc = c(0.05, 0.10, 0.15, 0.20, 0.25),
+                     actn = c(0.05, 0.10, 0.15, 0.20, 0.25)),
+                function(x) {
+                  SampleVarn(unique(diabetes_final$atc_5), x)
+                }
+)
+
+## Combine variation around A10B effects into single estimate 
+# from trial, drug and class (A10BA, A10BB etc)
+diabetes_final_smpls <- diabetes_final[rep(seq_along(diabetes_final$nct_id),1000),
+                                       c("nct_id", "drug", "atc_5") ]
+CombineVarn <- function(component = "cept", choice = list(atc5 = "0.25",
+                                                       drug = "0.25",
+                                                       trial = "0.25")){
+   atc5[[component]][diabetes_final_smpls$atc_5,  choice$atc5] +
+   drug[[component]][diabetes_final_smpls$drug, choice$drug] +
+  trial[[component]][diabetes_final_smpls$nct_id, choice$trial]
+}
+diabetes_final_smpls$cept <- CombineVarn()
+diabetes_final_smpls$como <- CombineVarn("como", list(atc5 = "0.5",
+                                                            drug = "0.5",
+                                                            trial = "0.5"))
+diabetes_final_smpls$allc <- CombineVarn("allc", list(atc5 = "0.15",
+                                                            drug = "0.2",
+                                                            trial = "0.25"))
+diabetes_final_smpls$actn <- CombineVarn("actn", list(atc5 = "0.05",
+                                                            drug = "0.1",
+                                                            trial = "0.15"))
+
+## Convert effects into marginal estimates of difference
+## from A10 effect (eg comoribdity = intercept + comorbidity) 
+diabetes_final_smpls_obs <- within(diabetes_final_smpls,{
+  cept <- cept
+  como <- cept + como
+  allc <- cept + allc
+  actn <- cept + como + actn
 })
-names(varn_res) <- names(varn_scen)
-cumprod(sapply(varn_res, length) ) # gives us 10,000 scenarios alone
-# At this point using <50MB
-rm(list = c("param_smpls", "params"))
 
- # Reshapes arrays to a vector, where each array contributes one element in turn
-  # The vector goes a1, b1, c1, d1, a2, b2, c2, d2, etc
-  # First each array is collapsed, it goes columns, rows, slices etc
-  # Then a matrix is created, which is again collapsed column wise
-  # Final ordering is dataset 1:1000 (rows), for class 1(cols), for trial 1(slices),
-  # then dataset 1:1000 for class 2, trial 1
-  # then dataset 1:1000 for class 3, trial 1  etc
-varn_res <- lapply(varn_res, function (x) { # apply to each parameter
-  lapply(x, c)}) # apply to each combination
-# 
-# ### Make labelling function 
-# MakeLabs <- function(datasets = 1000, drug_classes = 6, trials = 10){
-#   my_array <- array(NA, dim = c(datasets, drug_classes, trials))
-#   my_array_lbl <- my_array
-#   for (i in 1:datasets){
-#     for(j in 1:drug_classes){
-#       for(k in 1:trials){
-#         my_array_lbl[i,j,k] <- paste0("dataset", i, "_", "class", j, "_", "trial", k)
-#       }
-#     }
-#   }
-#   c(my_array_lbl)
-# }
-# 
-# my_df <- data.frame(lbls = MakeLabs(), res = varn_res$intrcpt[[1]] )
+diabetes_final_smpls_obs2 <- within(diabetes_final_smpls, {
+  cept = cept + main_scen$cept[1]
+  como = como + main_scen$como[1]
+  allc = allc + main_scen$allc[1]
+  actn = actn + main_scen$actn[1]
+})
+
+
+main_scen_obs <- within(main_scen, {
+  cept <- cept
+  como <- cept+como
+  allc <- cept+allc
+  actn <- cept + como + actn
+})
+
+diabetes_final_smpls_obs2 <- within(diabetes_final_smpls2, {
+  cept = cept + main_scen$cept[1]
+  como = como + main_scen$como[1]
+  allc = allc + main_scen$allc[1]
+  actn = actn + main_scen$actn[1]
+})
+
+
+### Calcualate error terms based on SD
+# Standardised sd is approx 1 for change in HbA1c
+comorbidity_prev <- 0.2
+sd <- 1
+diabetes_final_se <- within(diabetes_final, {
+  ncomo_se = sd/ ((1-comorbidity_prev) * diabetes_final$n_per_grp)^0.5
+  ycomo_se = sd/ (   comorbidity_prev  * diabetes_final$n_per_grp)^0.5
+})
+
+diabetes_final_se$inter_se <- with(diabetes_final_se,{
+  como_se^2 + ycomo_se^2
+})
+
+
+

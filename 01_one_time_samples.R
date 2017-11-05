557474f040912d8267374116aafb3e6efc818bdf
David A McAllister <David.McAllister@glasgow.ac.uk>
2017-11-05 22:55:50 +0000

    Rerun looping through scenarios

--- 01_one_time_samples.R
+++ 01_one_time_samples.R
@@ -2,7 +2,7 @@
 # Initially generates random samples from normal distribution. THese will be the same for each scenario
 # Next Calculates overall group effect and variance for each component that will make-up the final scenarios
 # eg (intercept, comorbidity effect, treatment allocation and interaction)
-
+library(tidyverse)
 ## Read in diabetes trials
 load("../Trial_identify/clinical_trials_august_2017/scratch_data/data_for_simulation.Rdata")
 
@@ -73,9 +73,45 @@ atc5 <- lapply(list(cept = c(0.25, 0.5),
 
 ## Combine variation around A10B effects for intercept, comorbidity, allocation and itneraction
 ## into single estimate from trial, drug and class (A10BA, A10BB etc)
-diabetes_final_smpls <- diabetes_final[rep(seq_along(diabetes_final$nct_id), each = 1000),
-                                       c("nct_id", "drug", "atc_5") ]
-diabetes_final_smpls$iter <- 1:1000
+
+atc5 <- atc5$actn
+atc5_names <- rownames(atc5)
+colnames(atc5) <- paste("atc5", colnames(atc5), sep = "_")
+atc5 <- as_tibble(atc5) %>% 
+  mutate(atc_5 = atc5_names)
+
+drug <- drug$actn
+drug_names <- rownames(drug)
+colnames(drug) <- paste("drug", colnames(drug),  sep = "_")
+drug <- as_tibble(drug) %>% 
+  mutate(drug = drug_names)
+
+trial <- trial$actn
+trial_names <- rownames(trial)
+colnames(trial) <- paste("trial", colnames(trial), sep = "_")
+trial <- as_tibble(trial) %>% 
+  mutate(nct_id = trial_names)
+
+trial <- trial %>% 
+  arrange(nct_id)
+
+drug <- drug %>% 
+  inner_join(diabetes_final %>%  select(nct_id, drug)) %>% 
+  arrange(nct_id) %>% 
+  select(-nct_id)
+
+atc5 <- atc5 %>% 
+  inner_join(diabetes_final %>%  select(nct_id, atc_5)) %>% 
+  arrange(nct_id) %>% 
+  select(-nct_id)
+
+actn <- bind_cols(atc5, trial, drug) %>% 
+  arrange(atc_5, drug, nct_id) %>% 
+  mutate(iteration = rep(1:1000, 161)) %>% 
+    select(atc_5, drug, nct_id, iteration, everything())
+saveRDS(actn, file = "scratch_data/interactn_opts.Rds")
+save(diabetes_final, file = "scratch_Data/interaction_opts_ordering.Rds")
+  # inner_join(trial)
 # 
 # CombineVarn <- function(component = "cept", choice = list(atc5 = "0.25",
 #                                                        drug = "0.25",
--- 02_create_interaction_datasets.R
+++ 02_create_interaction_datasets.R
@@ -1,122 +1,47 @@
 # 02_create_interaction_datasets.R
 # Create effect estimates interactions with variation at trial, drug and class level
-atc5 <- atc5["actn"]
-drug <- drug["actn"]
-trial <- trial["actn"]
+diabetes <- readRDS("scratch_data/interactn_opts.Rds")
 
 count <- 0
-res <- matrix(nrow = nrow(diabetes_final_smpls), ncol = 5^3)
+res <- matrix(nrow = nrow(diabetes), ncol = 5^3)
 res_names <- vector(length = 5^3)
-for (i in 1:5){
-  for(j in 1:5){
-    for(k in 1:5){
+
+for (i in c("atc5_0.05", "atc5_0.1", "atc5_0.15", "atc5_0.2", "atc5_0.25")){
+  for(j in c("trial_0.05", "trial_0.1", "trial_0.15", "trial_0.2", "trial_0.25")){
+    for(k in c("drug_0.05", "drug_0.1", "drug_0.15", "drug_0.2", "drug_0.25")){
       count <- count + 1
     res[, count] <- 
-       atc5$actn[diabetes_final_smpls$atc_5,  c("0.05", "0.1", "0.15", "0.2", "0.25")[i]] +
-       drug$actn[diabetes_final_smpls$drug,   c("0.05", "0.1", "0.15", "0.2", "0.25")[j]] +
-      trial$actn[diabetes_final_smpls$nct_id, c("0.05", "0.1", "0.15", "0.2", "0.25")[k]] 
-    res_names[count] <- paste(c("0.05", "0.1", "0.15", "0.2", "0.25")[i],
-                       c("0.05", "0.1", "0.15", "0.2", "0.25")[j],
-                       c("0.05", "0.1", "0.15", "0.2", "0.25")[k],
-                       sep = "_")
-    }
+       diabetes[ , i] +
+       diabetes[ , j] +
+       diabetes[ , k] 
+    res_names[count] <- paste(i, j, k, sep = "_")
+      }
   }
 }
-
 colnames(res) <- res_names
+rownames(res) <- paste(diabetes$atc_5, diabetes$drug, diabetes$nct_id, diabetes$iteration,
+                       sep = "_")
 
-scen1 <- diabetes_final_smpls
-
-# Add in main effect to a chosen variation scenario
-scen1$res <- res[, "0.1_0.1_0.1"] + -0.2
-
-# Choose an iteration
-scen1_iter1 <- scen1[scen1$iter == 1,]
-
-# Add in se term for that iteration
+# Add in se term for interaction
 comorbidity_prev <- 0.2
 sd <- 1
 # calculate SE for comorbidity adn non-comorbidity group (same for placebo and treatment)
+## Read in diabetes trials
+load("../Trial_identify/clinical_trials_august_2017/scratch_data/data_for_simulation.Rdata")
+
+# Remove single alpha-glucosidase inhibitor trial
+diabetes_final <-  
+  subset(diabetes_final, diabetes_final$atc_5 != "A10BF")
+diabetes_final <- diabetes_final[with(diabetes_final, order(atc_5, drug, nct_id)),]
+
 ncomo_se = sd/ ((1-comorbidity_prev) * diabetes_final$n_per_grp)^0.5
 ycomo_se = sd/ (   comorbidity_prev  * diabetes_final$n_per_grp)^0.5
-# Calculate SE for interaction
+# Calculate SE for interaction, same for all
 inter_prec <- 1/(2*ncomo_se^2 + 2*ycomo_se^2)
 
 library(INLA)
 
-## Compare INLA
-my_drug_n <- as.numeric(as.factor(scen1_iter1$drug))
-study_id_n <- as.numeric(as.factor(as.character(scen1_iter1$nct_id)))
-atc5_n <- as.numeric(as.factor(as.character(scen1_iter1$atc_5)))
-
-my_data <- data.frame(y = scen1_iter1$res, y_prec = inter_prec, 
-                      trial = study_id_n,
-                      myatc4 = 1,
-                      myatc5 = atc5_n,
-                      mydrug = my_drug_n)
-
-
-##Model formula trial within ATC4
-myform1 <- y ~ -1 + # no intercept
-  myatc4 + # single coefficient for overall treatment effect (prior for this specified below)
-  f(trial, # Treatment in each trial (difference from overall treatment effect)
-    model = "iid", # Standard assumption as trials uncorrelated
-    hyper = list(prec = list(prior = "logtnormal", param = c(mean = 0, prec = 0.25)))) 
-      # Half-normal prior for difference between trials
-
-## linear combinations
-# lc_list <-lapply(1:161, function (x) {
-#   vect <- rep(NA, 161)
-#   vect[x] <- 1
-#   inla.make.lincomb(myatc4 = 1, trial = vect) 
-# })
-# lc_flat_list <- do.call(c, lc_list)
-# names(lc_flat_list) <- paste0("lc", 1:161)
-
-## Run model and specify rest of priors, and that likelihood fixed
-mod1 <- inla(myform1, 
-             data = my_data,
-              # Calculate linear combinations
- #            lincomb = lc_flat_list,
-            # control.inla(list(lincomb.derived.only = FALSE)), # Make more acurate estimate still half
-             # Likelihood distribution
-             family = "gaussian",
-             # Fix likelihood hyperpars as the data is fixed with known precision.
-             control.family = list(hyper = list(prec = list(fixed = TRUE, initial = 0))),
-             # Likelihood precisions
-             scale = my_data$y_prec,
-             # Prior distribution for "fixed" effects - really for mu_mu
-             control.fixed = list(mean = 0, prec = 0.25),
-             # Optionally compute DIC
-             verbose = FALSE)
-summary(mod1)
-
-## Run more complex model, trial within drug within ATC4 class
-
-myform_nested <- y ~ -1 + myatc4 + 
-  f(trial, model = "iid", 
-    hyper = list(prec = list(prior = "logtnormal", param = c(mean = 0, prec = 1)))) +
-  f(mydrug, model = "iid", 
-    hyper = list(prec = list(prior = "logtnormal", param = c(mean = 0, prec = 0.01))))
-
-mod1_nested <- inla(myform_nested, 
-             data = my_data,
-             # Add linear combinations to estimate drug-class
-             # lincomb = dc_all,
-             # Likelihood distribution
-             family = "gaussian",
-             # Fix likelihood hyperpars as the data is fixed with known precision.
-             control.family = list(hyper = list(prec = list(fixed = TRUE, initial = 0))),
-             # Likelihood precisions
-             scale = my_data$y_prec,
-             # Prior distribution for "fixed" effects - really for mu_mu
-             control.fixed = list(mean = 0, prec = 0.25),
-             # Optionally compute DIC
-             verbose = FALSE)
-summary(mod1_nested)
-
-
-## Run more complex model still, trial within drug within ATC5 class within ATC4 class
+## Write model
 myform_nested2 <- y ~ -1 + myatc4 + 
   f(trial, model = "iid", 
     hyper = list(prec = list(prior = "logtnormal", param = c(mean = 0, prec = 1)))) +
@@ -125,18 +50,60 @@ myform_nested2 <- y ~ -1 + myatc4 +
   f(myatc5, model = "iid", 
     hyper = list(prec = list(prior = "logtnormal", param = c(mean = 0, prec = 0.01))))
 
-mod1_nested2 <- inla(myform_nested2, 
-             data = my_data,
-             # Add linear combinations to estimate drug-class
-             # lincomb = dc_all,
-             # Likelihood distribution
-             family = "gaussian",
-             # Fix likelihood hyperpars as the data is fixed with known precision.
-             control.family = list(hyper = list(prec = list(fixed = TRUE, initial = 0))),
-             # Likelihood precisions
-             scale = my_data$y_prec,
-             # Prior distribution for "fixed" effects - really for mu_mu
-             control.fixed = list(mean = 0, prec = 0.25),
-             # Optionally compute DIC
-             verbose = FALSE)
-summary(mod1_nested2)
+## Make part of model matrix which is identical for all iterations
+my_drug_n <- as.numeric(as.factor(diabetes_final$drug))
+study_id_n <- as.numeric(as.factor(as.character(diabetes_final$nct_id)))
+atc5_n <- as.numeric(as.factor(as.character(diabetes_final$atc_5)))
+
+## Create dataset which is consistent for all iterations
+my_data <- data.frame(y_prec = inter_prec, 
+                      trial = study_id_n,
+                      myatc4 = 1,
+                      myatc5 = atc5_n,
+                      mydrug = my_drug_n)
+
+## Loop through 6 scenarios, this will take approximately 3 hours
+scenarios <- c("atc5_0.05_trial_0.05_drug_0.05",
+                  "atc5_0.1_trial_0.1_drug_0.1",
+                  "atc5_0.25_trial_0.25_drug_0.25",
+                  "atc5_0.1_trial_0.1_drug_0.25",
+                  "atc5_0.1_trial_0.25_drug_0.1",
+                  "atc5_0.25_trial_0.1_drug_0.1")
+
+for (choose_scenario in scenarios) {
+print(choose_scenario)
+# Add in main effect to a chosen variation scenario
+ diabetes$res <- res[, choose_scenario] + -0.2
+
+# Loop through each iteration
+ scenario <- vector(length = 250, mode = "list")
+ for (iter in 1:250){
+      
+    ## Add values for specific iteration
+    my_data$y <-  diabetes$res[diabetes$iteration == iter]
+    
+    ## Run model, trial within drug within ATC5 class within ATC4 class
+    mod1_nested2 <- inla(myform_nested2, 
+                 data = my_data,
+                 # Add linear combinations to estimate drug-class
+                 # lincomb = dc_all,
+                 # Likelihood distribution
+                 family = "gaussian",
+                 # Fix likelihood hyperpars as the data is fixed with known precision.
+                 control.family = list(hyper = list(prec = list(fixed = TRUE, initial = 0))),
+                 # Likelihood precisions
+                 scale = my_data$y_prec,
+                 # Prior distribution for "fixed" effects - really for mu_mu
+                 control.fixed = list(mean = 0, prec = 0.25),
+                 # Optionally compute DIC
+                 verbose = FALSE)
+    # Storing iterations in a single list
+    scenario[[iter]] <- summary(mod1_nested2)
+ }
+ # Saving each list as a data file
+ saveRDS(scenario, file = paste0("simulate_interactions/scenario_",choose_scenario, ".rds" ))
+}
+# 
+# 
+# fxd <- lapply(scenario, function(x) x$fixed)
+# fxd <- do.call(rbind, fxd)

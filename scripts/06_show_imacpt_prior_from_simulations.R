7b2b9da42df70678ab948da481ff9bbc470882f4
David A McAllister <david.mcallister@glasgow.ac.uk>
2018-09-12 14:44:54 +0100

    Some fake dat to test getting correct linear combinations out

--- scripts/06_show_imacpt_prior_from_simulations.R
+++ scripts/06_show_imacpt_prior_from_simulations.R
@@ -32,7 +32,7 @@ load(file = "data/sim1/std/for_inla.Rdata")
 
 ## NEW CLASS (dc1) NEW DRUG IN NEW CLASS (dc2) NEW DRUG IN EXISTING CLASS (dc3) 
 
-diabetes_abc <- diabetes%>% 
+diabetes_abc <- diabetes %>% 
   distinct(atc_5, drug) %>%
   mutate(newdrug = paste0(row_number(),drug))
 
@@ -58,6 +58,13 @@ mdls <- map(1:3, function (i) {
   no_a10bx %>% 
     distinct(atc_5, drug)
   
+  ###DM##
+  # The drug is set to NA becuase all of the data in that class is set to NA
+  no_a10bx %>%  filter(atc_5 == "A10BX") %>%  distinct(atc_5, nct_id,  drug, res)
+#     atc_5      nct_id          drug res
+# 1 A10BX NCT00519142 22mitiglinide  NA
+# 2 A10BX NCT00097786 23nateglinide  NA
+# 3 A10BX NCT00568984 24repaglinide  NA
   # Make linear combination
   dc1 <- inla.make.lincomb(myatc4 = 1, myatc5 = c(NA, NA, NA, NA, NA, NA, 1)) 
   
@@ -65,6 +72,37 @@ mdls <- map(1:3, function (i) {
                             mydrug = c(rep(NA, 23), 1)) 
   dc3 <- inla.make.lincomb(myatc4 = 1, myatc5 = c(NA, NA, NA, NA, NA, 1, NA), 
                            mydrug = c(rep(NA, 20), 1, rep(NA,3))) 
+  
+  ## Bit of code to create list of vectors for different linear combinations
+  mydrug_vect_inld <- map(1:6, function(x){
+    mydrug_vect <- rep(NA, 6)
+    myatc5_vect <- c(NA, NA)
+    
+    mydrug_vect[x] <- 1
+    if(x <=3) myatc5_vect[1] <- 1
+    if(x>=4) myatc5_vect[2] <- 1
+    list(myatc5_vect, mydrug_vect)
+    })
+ names(mydrug_vect_inld) <- paste0("i", 1:6)
+ mydrug_vect_inld <- transpose(mydrug_vect_inld)   
+ names(mydrug_vect_inld) <- c("myatc5_vect", "mydrug_vect") 
+myatc5_vect_incld <- map()
+  ## create linear combinations
+ dc_tests <- map2(mydrug_vect_inld$myatc5_vect, mydrug_vect_inld$mydrug_vect, function(atc5, drug){
+    inla.make.lincomb(myatc4 =1, myatc5 = atc5, mydrug = drug)
+ })
+  
+ ## create fake data
+  my_data_test <- my_data %>% 
+    filter(myatc5 <=2) %>% 
+    group_by(myatc4, myatc5) %>% 
+    slice(1:3) %>% 
+    ungroup() %>% 
+    mutate(y_prec = rep(100, 6),
+           # Can vary the following to test what happens with INLA when given a dataframe
+           y = c(-1, -0.5, 0, 0.5, 1, 2),
+           trial = 1:6,
+           mydrug = 1:6)
   #   
   # Select iteration
   ## Add values for specific iteration
@@ -72,16 +110,17 @@ mdls <- map(1:3, function (i) {
   
   ## Run model, trial within drug within ATC5 class within ATC4 class
   mod1_nested2 <- inla(myform_nested2, 
-                       data = my_data,
+                       data = my_data_test,
                        # Add linear combinations to estimate drug-class
                        #lincomb = dc1,
-                       lincomb = c(a = dc1, b = dc2, c= dc3),
+                       # lincomb = c(a = dc1, b = dc2, c= dc3),
+                       lincom = do.call(c, dc_tests),
                        # Likelihood distribution
                        family = "gaussian",
                        # Fix likelihood hyperpars as the data is fixed with known precision.
                        control.family = list(hyper = list(prec = list(fixed = TRUE, initial = 0))),
                        # Likelihood precisions
-                       scale = my_data$y_prec,
+                       scale = my_data_test$y_prec,
                        # Prior distribution for "fixed" effects - really for mu_mu
                        control.fixed = list(mean = 0, prec = 0.25),
                        # Optionally compute DIC

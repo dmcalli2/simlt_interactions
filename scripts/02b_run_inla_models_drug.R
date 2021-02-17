f690373d6ef2e941f07bf019a6e33ba79d797306
dmcalli2 <david.mcallister@glasgow.ac.uk>
2021-02-17 07:41:13 +0000

    add in wrapper so fails gracefully

--- scripts/02b_run_inla_models_drug.R
+++ scripts/02b_run_inla_models_drug.R
@@ -7,6 +7,95 @@ with_args <- TRUE
 argsd <- commandArgs(trailingOnly=TRUE)
 print(argsd)
 
+### Add in safely function as alternative to purrs https://gitlab.com/hrbrmstr/freebase/-/blob/master/inst/templates/safely.R
+# Less cool counterparts to purrr's side-effect capture-rs
+#
+# Most of the helper functions are 100% from output.R in purrr repo
+#
+# @param quiet Hide errors (`TRUE`, the default), or display them
+#   as they occur?
+# @param otherwise Default value to use when an error occurs.
+#
+# @return `safely`: wrapped function instead returns a list with
+#   components `result` and `error`. One value is always `NULL`.
+#
+#   `quietly`: wrapped function instead returns a list with components
+#   `result`, `output`, `messages` and `warnings`.
+#
+#   `possibly`: wrapped function uses a default value (`otherwise`)
+#   whenever an error occurs.
+hpccsafely <- function(.f, otherwise = NULL, quiet = TRUE) {
+  function(...) capture_error(.f(...), otherwise, quiet)
+}
+quietly <- function(.f) {
+  function(...) capture_output(.f(...))
+}
+possibly <- function(.f, otherwise, quiet = TRUE) {
+  force(otherwise)
+  function(...) {
+    tryCatch(.f(...),
+             error = function(e) {
+               if (!quiet)
+                 message("Error: ", e$message)
+               otherwise
+             },
+             interrupt = function(e) {
+               stop("Terminated by user", call. = FALSE)
+             }
+    )
+  }
+}
+capture_error <- function(code, otherwise = NULL, quiet = TRUE) {
+  tryCatch(
+    list(result = code, error = NULL),
+    error = function(e) {
+      if (!quiet)
+        message("Error: ", e$message)
+      
+      list(result = otherwise, error = e)
+    },
+    interrupt = function(e) {
+      stop("Terminated by user", call. = FALSE)
+    }
+  )
+}
+capture_output <- function(code) {
+  warnings <- character()
+  wHandler <- function(w) {
+    warnings <<- c(warnings, w$message)
+    invokeRestart("muffleWarning")
+  }
+  
+  messages <- character()
+  mHandler <- function(m) {
+    messages <<- c(messages, m$message)
+    invokeRestart("muffleMessage")
+  }
+  
+  temp <- file()
+  sink(temp)
+  on.exit({
+    sink()
+    close(temp)
+  })
+  
+  result <- withCallingHandlers(
+    code,
+    warning = wHandler,
+    message = mHandler
+  )
+  
+  output <- paste0(readLines(temp, warn = FALSE), collapse = "\n")
+  
+  list(
+    result = result,
+    output = output,
+    warnings = warnings,
+    messages = messages
+  )
+}
+hpccinla <- hpccsafely(inla)
+
 como_prev <- ifelse(with_args, argsd[2] ,"std")
 
 load(file = paste0("data/sim1/",como_prev,"/for_inla.Rdata"))
@@ -42,7 +131,7 @@ print(como_prev)
       sel_drug_mydata$mydrug <- 1 #Have to recode to 1 in order to be consistent across drugs
       
       ## Run model, trial within drug 
-      mod1_nested3 <- inla(myform_nested3, 
+      mod1_nested3 <- hpccinla(myform_nested3, 
                            data = sel_drug_mydata,
                            #data = my_data_test,            #Use instead if testing 
                            # Add linear combinations to estimate drug-class
@@ -61,7 +150,11 @@ print(como_prev)
                            verbose = FALSE,
                            control.compute = list(config=TRUE,dic = TRUE, waic = TRUE),
                            control.inla = list())
-      
+      if(is.null(mod1_nested3$error)) {
+        mod1_nested3 <- mod1_nested3$result } else{
+          mod1_nested3 <- "error in INLA"
+        }
+
       drug_models[[j]] <- summary(mod1_nested3)
     }
     drugs[[iter]] <- drug_models
